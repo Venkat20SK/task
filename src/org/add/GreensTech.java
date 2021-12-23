d9f5db41b9db6f49a9f404460f1ec1f4c8caf65e
Venkat20SK <venkattram143@gmail.com>
2021-12-23 00:58:39 -0800

    greens

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+public class GreensTech {
+private void greensOmr() {
+	System.out.println("1");
+
+}
+private void greensAdayar() {
+	System.out.println("2");
+
+}
+private void greensThambaram() {
+	System.out.println("3");
+
+}
+private void greensVelacherry() {
+	System.out.println("4");
+
+}
+private void grennsAnnanagar() {
+	System.out.println("5");
+
+}
+public static void main(String[] args) {
+	GreensTech v =new GreensTech();
+	v.greensOmr();
+	v.greensAdayar();
+	v.greensThambaram();
+	v.greensVelacherry();
+	v.grennsAnnanagar();
+	
+}
+}
